3a04d4316e1364054ad7e1a08c616d92971dbb2b
Élise <elise@eiss.fr>
2024-05-06 21:58:51 +0200

    WIP with args

--- src/bin/bus.rs
+++ src/bin/bus.rs
@@ -1,73 +1,7 @@
 use std::error::Error;
 
-use bus_20240330::timetable::{parse::parse_files, runs::runs_on_date, Journey};
-
-fn mk_filter_for_stop(
-    stop_id: usize,
-    today: time::Date,
-    now: time::Time,
-) -> Option<Box<dyn Fn(&Journey) -> bool>> {
-    let time = now - time::Duration::minutes(10);
-    let fun = move |journey: &Journey| {
-        if let Some(stop) = journey.stops.iter().find(|stop| stop.stop_idx == stop_id) {
-            runs_on_date(&today, journey.oparates) && stop.time > time
-        } else {
-            false
-        }
-    };
-
-    return Some(Box::new(fun));
-}
+use bus_20240330::run;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let timetable = match parse_files() {
-        Ok(tt) => tt,
-        Err(err) => return Err(err),
-    };
-
-    use inquire::{error::InquireError, Select};
-
-    let ans: Result<&str, InquireError> = Select::new(
-        "Depart from?",
-        timetable
-            .stop_names
-            .iter()
-            .map(|name| name.as_str())
-            .collect(),
-    )
-    .prompt();
-    let start_stop = ans.unwrap();
-    let start_stop_id = timetable.get_stop_id(start_stop).unwrap();
-    let now_date = time::OffsetDateTime::now_local().unwrap();
-    // let now_date = time::macros::datetime!(2024 - 04 - 24 17:26);
-    let today = now_date.date();
-    let now_time = now_date.time();
-    // let filter = mk_filter_for_stop(start_stop_id, today, now_time).unwrap();
-    timetable
-        .journeys
-        .iter()
-        .filter_map(|journey| {
-            if let Some(stop) = journey
-                .stops
-                .iter()
-                .find(|stop| stop.stop_idx == start_stop_id)
-            {
-                Some((journey, stop))
-            } else {
-                None
-            }
-        })
-        .filter(|(journey, stop)| {
-            let time = now_time - time::Duration::minutes(10);
-            runs_on_date(&today, journey.oparates) && stop.time > time
-        })
-        .take(3)
-        .for_each(|(journey, stop)| {
-            if stop.time < now_time {
-                println!("(should have already departed)");
-            }
-            journey.pretty_print_from_stop_id(&timetable.stop_names, start_stop_id);
-            println!("\n");
-        });
-    Ok(())
+    run()
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,73 @@
 pub mod clock_time;
 pub mod timetable;
+
+use clap::Parser;
+use serde::{Deserialize, Serialize};
+
+#[derive(Parser, Serialize, Deserialize)]
+struct Opt {
+    depart_from: Option<String>,
+    number_to_show: Option<usize>,
+}
+
+fn get_departure_stop<'a>(opt: &Opt, stops: impl Iterator<Item = &'a str>) -> Option<String> {
+    if let Some(depart_from) = &opt.depart_from {
+        Some(depart_from.to_owned())
+    } else {
+        use inquire::{error::InquireError, Select};
+
+        let ans: Result<&str, InquireError> = Select::new("Depart from?", stops.collect()).prompt();
+        if let Ok(ans) = ans {
+            Some(ans.to_owned())
+        } else {
+            None
+        }
+    }
+}
+
+pub fn run() -> Result<(), Box<dyn std::error::Error>> {
+    let opt = Opt::parse();
+    let tt = timetable::parse::parse_files()?;
+    let Some(depart_from) =
+        get_departure_stop(&opt, tt.stop_names.iter().map(|item| item.as_str()))
+    else {
+        // TODO: actually return an error
+        return Ok(());
+    };
+    let number_to_show = opt.number_to_show.unwrap_or(3);
+    // let datetime = time::OffsetDateTime::now_local().unwrap();
+    let datetime = time::macros::datetime!(2024 - 05 - 06 08:00);
+    let today = datetime.date();
+    let now = datetime.time();
+    aaa(tt, depart_from.as_str(), today, now);
+    Ok(())
+}
+
+fn aaa(tt: timetable::TimeTable, start_stop_name: &str, day: time::Date, clock_time: time::Time) {
+    let start_stop_id = tt.get_stop_id(start_stop_name).unwrap();
+    tt.journeys
+        .iter()
+        .filter_map(|journey| {
+            if let Some(stop) = journey
+                .stops
+                .iter()
+                .find(|stop| stop.stop_idx == start_stop_id)
+            {
+                Some((journey, stop))
+            } else {
+                None
+            }
+        })
+        .filter(|(journey, stop)| {
+            let time = clock_time - time::Duration::minutes(10);
+            timetable::runs::runs_on_date(&day, journey.oparates) && stop.time > time
+        })
+        .take(3)
+        .for_each(|(journey, stop)| {
+            if stop.time < clock_time {
+                println!("(should have already departed)");
+            }
+            journey.pretty_print_from_stop_id(&tt.stop_names, start_stop_id);
+            println!("\n");
+        });
+}
